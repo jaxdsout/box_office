c7ab7f4ff6b8d6ec191e877e1823c56d10562383
jaxdsout <jaxon.southern@mac.com>
2024-07-29 13:34:48 -0500

    created year filter and treemap for data

--- loader.py
+++ loader.py
@@ -4,11 +4,13 @@ import pandas as pd
 
 def load_data(directory):
     data_frames = []
+
     for file_name in os.listdir(directory):
         if file_name.endswith(".csv"):
             file_path = os.path.join(directory, file_name)
             df = pd.read_csv(file_path)
-            df['year'] = file_name.split('_')[-1].split('.')[0]  # Extract year from filename
+            df['year'] = file_name.split('_')[-1].split('.')[0]
             data_frames.append(df)
+
     combined_data = pd.concat(data_frames, ignore_index=True)
     return combined_data
--- visualizer.py
+++ visualizer.py
@@ -1,8 +1,43 @@
 import streamlit as st
+import plotly.express as px
 import pandas as pd
 from loader import load_data
+from datetime import date
 
 path = "datasets"
-data = load_data(path)
+df = load_data(path)
 
 st.title("Box Office Data")
+
+years = sorted(df['year'].unique(), reverse=True)
+default_year_index = years.index('2024')
+
+selected_year = st.selectbox(
+    label="Year",
+    options=years,
+    index=default_year_index
+)
+
+filtered_df = df[df['year'] == selected_year]
+
+figure = px.treemap(
+    filtered_df,
+    path=[px.Constant(selected_year), "title"],
+    values='worldwide'
+)
+
+figure.update_traces(root_color="black")
+figure.update_layout(margin=dict(t=50, l=25, r=25, b=25))
+
+st.plotly_chart(
+    figure,
+    theme="streamlit"
+)
+
+# st.bar_chart(
+#     filtered_df,
+#     x='worldwide',
+#     y='title'
+# )
+
+
